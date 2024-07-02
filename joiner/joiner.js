5e6572dd42f2c2b11e19bb539a68975174d111ef
Demo is programming <68935450+b07505054@users.noreply.github.com>
2024-07-02 15:15:58 +0800

    Add files via upload

---
+++ joiner/joiner.js
@@ -0,0 +1,94 @@
+const webSocket = new WebSocket("ws://192.168.0.118:3000")
+
+webSocket.onmessage = (event) => {  //傾聽傳入資料
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "offer":
+            peerConn.setRemoteDescription(data.offer)
+            createAndSendAnswer()
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+function createAndSendAnswer () {
+    peerConn.createAnswer((answer) => {
+        peerConn.setLocalDescription(answer)
+        sendData({
+            type: "send_answer",
+            answer: answer
+        })
+    }, error => {
+        console.log(error)
+    })
+}
+
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+let username
+
+function joinCall() {
+
+    username = document.getElementById("username-input").value
+
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls":["stun:stun.aa.net.uk:3478","stun:stun.cope.es:3478","stun:stun.demos.ru:3478"]
+                }
+            ]
+        }
+
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            
+            sendData({
+                type: "send_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        sendData({
+            type: "join_call"
+        })
+
+    }, (error) => {
+        console.log(error)
+    })
+}
+
---
+++ roomer/roomer.js
@@ -0,0 +1,94 @@
+const webSocket = new WebSocket("ws://192.168.0.118:3000")
+
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+let username
+function sendUsername() {
+
+    username = document.getElementById("username-input").value
+    sendData({
+        type: "store_user"
+    })
+}
+
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+function startCall() {
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls":["stun:stun.aa.net.uk:3478","stun:stun.cope.es:3478","stun:stun.demos.ru:3478"]
+                }
+            ]
+        }
+
+        peerConn = new RTCPeerConnection(configuration)  //
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+function createAndSendOffer() {
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
+
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
